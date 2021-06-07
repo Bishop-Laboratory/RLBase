d8aadd14f8dd66db1b258c1d84624f7c4b281df9
millerh1 <millerh1@livemail.uthscsa.edu>
2021-06-07 15:48:00 -0500

    fixed bug in workflow

--- RMapDB-API/tests/test_db.py
+++ RMapDB-API/tests/test_db.py
@@ -22,7 +22,7 @@ def test_init_db_command(runner, monkeypatch):
     def fake_init_db():
         Recorder.called = True
 
-    monkeypatch.setattr('fibrodb.db.init_db', fake_init_db)
+    monkeypatch.setattr('rmapdb.db.init_db', fake_init_db)
     result = runner.invoke(args=['init-db'])
     assert 'Initialized' in result.output
     assert Recorder.called
