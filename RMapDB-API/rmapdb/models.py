a1f6c7d8276ad246d4e5ab91f131359cf41766e4
Awsthyn <aguswagner008@gmail.com>
2021-06-25 18:29:14 -0300

    three bar charts

--- RMapDB-API/rmapdb/models.py
+++ RMapDB-API/rmapdb/models.py
@@ -12,7 +12,10 @@ Record = Base.classes.sample_data
 featureList = ["GSM", "Cell", "SRX","mode","Species","Condition","Group","Genotype",
 '3UTR__Log2 Ratio (obs/exp)','5UTR__Log2 Ratio (obs/exp)',
 'TTS__Log2 Ratio (obs/exp)','Exon__Log2 Ratio (obs/exp)','Intron__Log2 Ratio (obs/exp)',
-'Intergenic__Log2 Ratio (obs/exp)']
+'Intergenic__Log2 Ratio (obs/exp)', 'Retroposon__Log2 Ratio (obs/exp)', 'SINE__Log2 Ratio (obs/exp)',
+ 'Low_complexity__Log2 Ratio (obs/exp)','Simple_repeat__Log2 Ratio (obs/exp)',
+ 'rRNA__Log2 Ratio (obs/exp)','RNA__Log2 Ratio (obs/exp)','ncRNA__Log2 Ratio (obs/exp)',
+ 'RC__Log2 Ratio (obs/exp)','pseudo__Log2 Ratio (obs/exp)','scRNA__Log2 Ratio (obs/exp)']
 
 serialization = []
 for feature in featureList:
--- client/src/pages/RLoop/index.tsx
+++ client/src/pages/RLoop/index.tsx
@@ -4,16 +4,33 @@ import BarChart from "../../components/BarChart";
 import { RouteComponentProps } from "react-router-dom";
 import axios from "axios";
 
-const initialState = [{x:"",y:0},{x:"", y: 0},{x:"",y:0},{x:"", y: 0},{x:"",y:0},{x:"", y: 0},{x:"",y:0},{x:"", y: 0},]
+const initialState = [
+  { x: "", y: 0 },
+  { x: "", y: 0 },
+  { x: "", y: 0 },
+  { x: "", y: 0 },
+  { x: "", y: 0 },
+  { x: "", y: 0 },
+  { x: "", y: 0 },
+  { x: "", y: 0 },
+];
 
 const convertData = (row: any) => {
   const KEYS = [
     "3UTR",
-    "TTS",
     "Exon",
-    "Intron",
     "Intergenic",
     "5UTR",
+    "Retroposon",
+    "SINE",
+    "Low_complexity",
+    "Simple_repeat",
+    "rRNA",
+    "RNA",
+    "ncRNA",
+    "RC",
+    "pseudo",
+    "scRNA",
   ];
   let array = KEYS.map((key) => {
     return {
@@ -24,43 +41,91 @@ const convertData = (row: any) => {
   return array;
 };
 
-
-function RLoop({location}:RouteComponentProps) {
-  
-  const [sampleData, setSampleData] =  useState<barChartDataItem[]>(initialState)
+function RLoop({ location }: RouteComponentProps) {
+  const [sampleData, setSampleData] = useState<barChartDataItem[]>(
+    initialState
+  );
+  const [minAndMax, setMinAndMax] = useState<number[]>([0, 0]);
   const [, /*loading*/ setLoading] = useState(false);
-  const [info, setInfo] = useState<any>({})
+  const [info, setInfo] = useState<any>({});
+  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
+
   useEffect(() => {
     const get = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
         const res: any = await axios.get(
           `http://127.0.0.1:5000/api/test/rloop-details${location.search}`
         );
-        if(res?.data?.[0]) {
-          setInfo(res.data[0])
-          setSampleData(convertData(res.data[0]))}
+        if (res?.data?.[0]) {
+          setInfo(res.data[0]);
+          const depuredData = convertData(res.data[0]);
+          const numericData = depuredData.map((item) => item.y);
+          setSampleData(depuredData);
+          setMinAndMax([Math.min(...numericData), Math.max(...numericData)]);
+        }
       } catch (error) {
         console.error(error);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
     };
     get();
   }, [location.search]);
   return (
     <>
-    <h3 className="mt-2 text-center">{info.srx}</h3>
-    <div className="d-flex mt-2">
-      <BarChart {...{selectedItem: sampleData}} />
+      <h3 className="mt-8 text-center">{info.SRX}</h3>
       <div>
-        <ul>
-          <li>Cell line/tissue: {info.cell}</li>
-          <li>Sample control accession(s): {info.control}</li>
-          <li>Genotype: {info.genotype}</li>
-        </ul>
+          <ul>
+            <li>Cell line/tissue: {info.cell}</li>
+            <li>Sample control accession(s): {info.control}</li>
+            <li>Genotype: {info.genotype}</li>
+          </ul>
+        </div>
+      <div>
+        <h3 className="mt-8 text-center">Genomic Feature Enrichment</h3>
+        <div className="d-flex w-100  justify-content-center mt-2">
+          <BarChart
+            {...{
+              title: "Gene Features",
+              selectedItem: sampleData.slice(0, 4),
+              minAndMax,
+              color: "blue",
+              setName,
+              setValue,
+            }}
+          />
+          <BarChart
+            {...{
+              title: "Repetitive Elements",
+              selectedItem: sampleData.slice(4, 9),
+              hideYAxis: true,
+              minAndMax,
+              color: "red",
+              setName,
+              setValue,
+            }}
+          />
+          <BarChart
+            {...{
+              title: "RNA Species",
+              selectedItem: sampleData.slice(9),
+              hideYAxis: true,
+              minAndMax,
+              color: "green",
+              setName,
+              setValue,
+            }}
+          />
+          <div
+            id="tooltip"
+            className="bg-dark text-light border border-info px-2 d-none position-absolute"
+          >
+            {name} | Log2 Ratio (obs/exp): {value}
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
