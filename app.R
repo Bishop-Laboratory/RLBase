dcbfc4636497571f1499a9d953e98422ed45888e
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-14 20:31:03 -0500

    added anno plots

--- app.R
+++ app.R
@@ -9,10 +9,25 @@ if (! "dataLst" %in% names(globalenv())) {
     load('dataLst.rda')
 }
 
-# Get the data for the RegioneR plots
-load("misc/report_rda/ERX2277510_E-MTAB-6318DRIP_mOHT_hg38.QC_report.rda")
-data_list$rlfs_data[[2]]$`regioneR::numOverlaps`$shifts
-
+# Get ammptatopms
+anno_data <- dataLst %>%
+    pluck("sample_quality_characteristics") %>%
+    filter(grepl(char_type, pattern = "__")) %>%
+    mutate(Annotation = gsub(char_type, pattern = "(.+)__(.+)", replacement = "\\1"),
+           data_type = gsub(char_type, pattern = "(.+)__(.+)", replacement = "\\2")) %>%
+    select(-char_type) %>%
+    inner_join(y = data.frame(
+        "Annotation" = c('Intergenic', 'Simple_repeat', 'Satellite', 'Promoter', 'pseudo',
+                         'Intron', 'TTS', 'LINE', 'LTR', 'SINE', 'DNA', 'CpG-Island', 'ncRNA',
+                         'Low_complexity', 'Exon', 'snRNA', 'Retroposon', '5UTR', '3UTR', 'srpRNA', 
+                         'tRNA', 'RC', 'scRNA', 'miRNA', 'RNA', 'snoRNA'),
+        "annotate_type" = c("gene", "rep", "rep", "gene", "RNA", "gene", "gene", 
+                            "rep", "rep", "rep", "rep", "gene", "RNA", "rep",
+                            "gene", "RNA", "rep", "gene", "gene", "RNA",
+                            "RNA", "RNA", "RNA", "RNA", "RNA", "RNA"), stringsAsFactors = FALSE
+    ), by = "Annotation") %>%
+    pivot_wider(id_cols = c(id, Annotation, annotate_type), names_from = data_type, values_from = value) %>%
+    inner_join(y = pluck(dataLst, "rmap_samples"), by = "id")
 
 # Define UI for application that draws a histogram
 ui <- function(request) {
@@ -98,7 +113,14 @@ ui <- function(request) {
                                     fluidRow(
                                         column(
                                             width = 12,
-                                            plotlyOutput('sampleAnnotationPlot')
+                                            selectInput(
+                                                inputId = 'chooseAnnoPlotData',
+                                                label = "Select Data Type",
+                                                choices = c("Log2 Ratio (obs/exp)",
+                                                            "LogP enrichment (+values depleted)"), 
+                                                selected = "Log2 Ratio (obs/exp)"
+                                            ),
+                                            plotOutput('sampleAnnotationPlot')
                                         )
                                     ),
                                     fluidRow(
@@ -208,7 +230,6 @@ server <- function(input, output, session) {
     
     
     ### Sample Page ###
-    
     # TODO: Should be sorted
     # TODO: Should contain quality score
     # TODO: Should contain link that will open the details modal
@@ -253,7 +274,7 @@ server <- function(input, output, session) {
         
         # Get file to load from
         current_file <-  current_samp %>%
-            list.files('misc/report_rda/', 
+            list.files('misc/report_rda_small/', 
                        pattern = .,
                        full.names = TRUE)
         
@@ -272,18 +293,93 @@ server <- function(input, output, session) {
                    "shift" = lz$shifts) %>%
             ggplot(aes(y = zscore, x = shift)) +
             geom_line() +
-            labs(title = "Peak enrichment around RLFS") +
+            labs(title = paste0(current_samp, ' RLFS analysis')) +
             ylab("Peak Enrichment (Z-Score)") +
             xlab("Distance to RLFS (bp)") +
             theme_bw(base_size = 15)
     })
     
     
-    
-    
-    
-    
-    
+    output$sampleAnnotationPlot <- renderPlot({
+        
+        opt <- input$chooseAnnoPlotData
+        
+        MIN_ALLOW <- ifelse(opt == "Log2 Ratio (obs/exp)", -7.5, -300)
+        MAX_ALLOW <- ifelse(opt == "Log2 Ratio (obs/exp)", 7.5, 300)
+        
+        print(MAX_ALLOW)
+        
+        # Get selected row from datatable
+        selectedRow <- ifelse(is.null(input$rmapSamples_rows_selected), 
+                              1, 
+                              input$rmapSamples_rows_selected)
+        
+        # Get current sample ID
+        current_samp <- dataLst %>%
+            pluck("rmap_samples") %>%
+            filter(row_number() == selectedRow) %>%
+            pull(id)
+        
+        
+        minplt <- (min(na.omit(anno_data)[, opt]) * 1.05 )%>% ifelse(. < MIN_ALLOW, MIN_ALLOW, .)
+        maxplt <- (max(na.omit(anno_data)[, opt]) * 1.05) %>% ifelse(. > MAX_ALLOW, MAX_ALLOW, .)
+        
+        print(maxplt)
+        
+        boxFills <- c('gene' = 'firebrick', 'RNA' = 'goldenrod', 'rep' = 'forestgreen')
+        titles <- c('gene' = 'Genomic Features', 'RNA' = 'ncRNAs', 'rep' = 'Repetitive Elements')
+        genelvls <- c("CpG-Island",
+                      "Promoter",
+                      "5UTR",
+                      "Exon",
+                      "Intron",
+                      "3UTR",
+                      "TTS",
+                      "Intergenic")
+        
+        lapply(unique(anno_data$annotate_type), function(annoNow) {
+            anno_data %>%
+                filter(annotate_type == !!annoNow) %>%
+                mutate(Annotation = if (!! annoNow == "gene") 
+                    factor(Annotation, levels =  genelvls) 
+                    else Annotation) %>%
+                mutate(sample_now = id == !! current_samp) %>%
+                fill(everything(0)) %>%
+                arrange(sample_now) %>%
+                ggplot(mapping = aes(x = Annotation, 
+                                     y = .data[[opt]])) +
+                geom_boxplot(fill = boxFills[annoNow],
+                             outlier.shape = NA) +
+                geom_jitter(mapping = aes(
+                    size = sample_now, 
+                    color = sample_now,
+                    alpha = sample_now, 
+                    ),
+                    width = 0.1) +
+                ggpubr::rremove("legend") +
+                ylab(opt) +
+                xlab(NULL) +
+                labs(title = titles[annoNow]) +
+                theme_bw() +
+                scale_color_manual(values = c(
+                    "TRUE" = "#2F3940",
+                    "FALSE" = "grey"
+                )) +
+                scale_alpha_manual(values = c(
+                    "TRUE" = 1,
+                    "FALSE" = .5
+                )) +
+                scale_size_manual(values = c(
+                    "TRUE" = 5,
+                    "FALSE" = .6
+                )) +
+                ggpubr::rremove("legend") +
+                theme(axis.text.x = element_text(angle = 45, vjust = 1,
+                                                 hjust = 1)) +
+                ylim(minplt, maxplt)
+        }) %>%
+            ggpubr::ggarrange(plotlist = ., nrow = 1, align = "h")
+    })
 }
 
 # TODO: Need URL cleaner
