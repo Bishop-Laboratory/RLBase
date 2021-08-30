343530accf05c698e59bc9a11ac09b46cfb6def2
millerh1 <millerh1@livemail.uthscsa.edu>
2021-08-30 11:44:47 -0500

    More testing

--- tests/shinytest.R
+++ tests/shinytest.R
@@ -1,4 +1,4 @@
-shinytest::testApp(".", compareImages = FALSE, interactive = FALSE, quiet = TRUE)
+shinytest::testApp(".", compareImages = FALSE)
 
 # shinytest::recordTest(loadTimeout = 1E5, seed = 1234)
 
