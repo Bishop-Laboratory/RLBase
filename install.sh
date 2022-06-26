a1e1de2e083d2c9f107bad701f611345c5179937
millerh1 <millerh1@livemail.uthscsa.edu>
2022-06-26 10:12:44 -0500

    Refactor for using renv

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt install -y libfontconfig1-dev
-R -e "install.packages(c('shiny','DT','plotly','aws.s3','shinycssloaders','shinyvalidate','futile.logger','kernlab','randomForest','rpart','MASS','prompter','shinyWidgets','pbapply','callr','VennDiagram','uuid','tidyverse','kableExtra','pheatmap', 'ggprism', 'bslib', 'RColorBrewer', 'BiocManager'), repos = 'https://packagemanager.rstudio.com/all/latest')"
-R -e "options(repos = c(RSPM = 'https://packagemanager.rstudio.com/all/latest')); BiocManager::install(c('RLSeq'))"
-
--- runApp.R
+++
@@ -1,3 +0,0 @@
-args <- commandArgs(trailingOnly = TRUE)
-
-shiny::runApp("rlbase/", port = as.numeric(args[1]), launch.browser = FALSE, host = "0.0.0.0")
