ff48529ef83ff8fba82e30376af9e6fc25af1cf4
millerh1 <millerh1@livemail.uthscsa.edu>
2022-06-26 09:26:05 -0500

    Add RSPM to install.sh to improve install speed

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt install -y libfontconfig1-dev
-R -e "options(repos = c(RSPM = 'https://packagemanager.rstudio.com/all/latest')); install.packages(c('shiny','DT','plotly','aws.s3','shinycssloaders','shinyvalidate','futile.logger','kernlab','randomForest','rpart','MASS','prompter','shinyWidgets','pbapply','callr','VennDiagram','uuid','tidyverse','kableExtra','pheatmap', 'ggprism', 'bslib', 'RColorBrewer', 'BiocManager'), repos = 'http://cran.rstudio.com/')"
+R -e "install.packages(c('shiny','DT','plotly','aws.s3','shinycssloaders','shinyvalidate','futile.logger','kernlab','randomForest','rpart','MASS','prompter','shinyWidgets','pbapply','callr','VennDiagram','uuid','tidyverse','kableExtra','pheatmap', 'ggprism', 'bslib', 'RColorBrewer', 'BiocManager'), repos = 'https://packagemanager.rstudio.com/all/latest')"
 R -e "options(repos = c(RSPM = 'https://packagemanager.rstudio.com/all/latest')); BiocManager::install(c('RLSeq'))"
 
